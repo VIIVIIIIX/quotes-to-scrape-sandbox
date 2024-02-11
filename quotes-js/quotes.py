d8ff9f1e80a68c2642f0cb7e071b9687f277279d
Udit Raval <157107304+VIIVIIIIX@users.noreply.github.com>
2024-02-11 17:53:32 +0530

    Create quotes.py

---
+++ quotes-js/quotes.py
@@ -0,0 +1,42 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def main():
+    main_url = 'https://quotes.toscrape.com'
+    
+    dict_list = list()
+    for n in range(1, 11):
+        page_url = main_url + f'/js/page/{n}/'
+        page_soup = get_soup(page_url)
+        for data in get_data(page_soup):
+            dict_list.append(data)
+
+    df = pd.DataFrame(dict_list)
+    df.to_csv('./quotes(js).csv', index=False)
+
+def get_soup(url):
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render()
+    soup = BeautifulSoup(r.html.html, 'html.parser')
+    return soup
+
+def get_data(soup):
+    for item in soup.find_all('div', {'class': 'quote'}):
+        quote = item.find('span', {'class': 'text'}).text
+        author = item.find('span', {'class': ''}).find('small', {'class': 'author'}).text
+        tags = ''
+        for tag in item.find('div', {'class': 'tags'}).find_all('a'):
+            if tags == '':
+                tags += tag.text
+            else:
+                tags += ',' + tag.text
+        yield {
+            'quote': quote,
+            'author': author,
+            'tags': tags
+            }        
+
+if __name__ == '__main__':
+    main()
