feb6e3a76540729889d14af1ab1fa767cc466fd7
Udit Raval <157107304+VIIVIIIIX@users.noreply.github.com>
2024-02-16 10:25:37 +0530

    Update and rename quotes.py to quotes-random.py

---
+++ quotes-random/quotes-random.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def main():
+    main_url = "https://quotes.toscrape.com/random" 
+    
+    info = list()
+    while len(info) != 100:
+        main_soup = get_soup(main_url)
+        for data in get_data(main_soup):
+            if data not in info:
+                info.append(data)
+    
+    df = pd.DataFrame(info)
+    df.to_csv("./quote-random/quote-random.csv", index=False)
+
+def get_soup(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup
+
+def get_data(soup):
+    for item in soup.find_all("div", {"class": "quote"}):
+        quote = item.find_all("span")[0].text
+        author = item.find_all("span")[1].small.text
+        tags = item.find("div", {"class": "tags"}).meta.get("content")
+
+        yield {
+            "quote": quote,
+            "author": author,
+            "tags": tags
+        }
+
+if __name__ == "__main__":
+    main()
