a64f61cbeeaa1a87f7b00f2d62f1e637ac588ad7
Udit Raval <157107304+VIIVIIIIX@users.noreply.github.com>
2024-04-15 19:05:44 +0530

    Create quotes-js.py

---
+++ quotes-js/quotes-js.py
@@ -0,0 +1,53 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+import csv
+
+
+@dataclass
+class Quote:
+    quote: str
+    author: str
+    tags: str
+
+
+def main():
+    url = "https://quotes.toscrape.com"
+
+    with open("./quotes-js.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["quote", "author", "tags"])
+
+        for num in range(1, 11):
+            soup = get_soup(url + f"/js/page/{num}")
+
+            for data in get_data(soup):
+                writer.writerow([data.quote, data.author, data.tags])
+
+                print(data)
+                print()
+
+    print("Quotes scraped successfully!")
+
+
+def get_soup(url):
+    session = HTMLSession()
+    response = session.get(url)
+    response.html.render(sleep=3)  # type: ignore
+    soup = BeautifulSoup(response.html.html, "html.parser")  # type: ignore
+    return soup
+
+
+def get_data(soup):
+    for item in soup.find_all("div", class_="quote"):
+        quote = item.find_all("span")[0].text.replace("“", "").replace("”", "")
+        author = item.find_all("span")[1].small.text
+        tags = ", ".join(
+            tag.text for tag in item.find("div", class_="tags").find_all("a")
+        )
+
+        yield Quote(quote=quote, author=author, tags=tags)
+
+
+if __name__ == "__main__":
+    main()
